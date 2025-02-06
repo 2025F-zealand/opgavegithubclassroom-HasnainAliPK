a1df75629333adb69365a771cf6372cc3788fb95
HasnainAliPK <hasnain2640@hotmail,com>
2025-02-06 12:57:40 +0100

    Jeg har added 3 tomme properties

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+    }
+}
